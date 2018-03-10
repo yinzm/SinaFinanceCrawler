d5c5331a49c593bcef3ff5fd3ca9d2b8f7b7a41e
yinzm <yinzongming@gmail.com>
2018-03-10 17:27:17 +0800

    fix some bug

--- sinaFinance/pipelines.py
+++ sinaFinance/pipelines.py
@@ -27,23 +27,25 @@ class DuplicatesPipeline(object):
         col = settings['MONGODB_COLLECTION']
         client = pymongo.MongoClient(host=host, port=port)
         db = client[dbName]
-        mongodb_data = db[col].find({}, {"link":1})
+        mongodb_data = db[col].find({}, {"link":1, "code":1})
 
         self.redis_db.flushdb() # 将redis中的数据进行清空
         if self.redis_db.hlen(self.redis_data_dict) == 0:
             for userId_dict in mongodb_data:
                 # 将mongodb中的数据插入到redis数据库中
-                self.redis_db.hset(self.redis_data_dict, userId_dict['link'], 0)
+                insert_str = userId_dict['code']+userId_dict['link']
+                self.redis_db.hset(self.redis_data_dict, insert_str, 0)
         client.close()
 
     def process_item(self, item, spider):
         # 如果该item已经在redis中出现了，那么丢弃
-        if self.redis_db.hexists(self.redis_data_dict, item['link']):
+        insert_str = item['code'] + item['link']
+        if self.redis_db.hexists(self.redis_data_dict, insert_str):
             raise DropItem("Duplicate item found: %s" % item)
             # raise DropItem("Duplicate item found!!!")
         else:
             # 如果没有出现过，那么将该item的信息插入到redis中
-            self.redis_db.hset(self.redis_data_dict, item['link'], 0)
+            self.redis_db.hset(self.redis_data_dict, insert_str, 0)
             return item
 
 class MongodbPipeline(object):
